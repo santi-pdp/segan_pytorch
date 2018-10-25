9d4c91a853816975158934f0a97883e855b0ec92
spascual <santi.pdp@gmail.com>
2018-10-25 11:42:21 +0200

    feat: add sinc_conv module from SincNet

--- segan/models/modules.py
+++ segan/models/modules.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import math
 import torch.nn.functional as F
 from torch.nn.utils.spectral_norm import spectral_norm
+import numpy as np
 
 
 def build_norm_layer(norm_type, param=None, num_feats=None):
@@ -191,8 +192,102 @@ class ResARModule(nn.Module):
         sh = self.forward_norm(sh, self.conv_1x1_res_norm)
         return y, sh
 
+# SincNet conv layer
+def flip(x, dim):
+    xsize = x.size()
+    dim = x.dim() + dim if dim < 0 else dim
+    x = x.contiguous()
+    x = x.view(-1, *xsize[dim:])
+    x = x.view(x.size(0), x.size(1), -1)[:, getattr(torch.arange(x.size(1)-1, 
+                      -1, -1), ('cpu','cuda')[x.is_cuda])().long(), :]
+    return x.view(xsize)
+
+
+def sinc(band,t_right, cuda=False):
+    y_right= torch.sin(2*math.pi*band*t_right)/(2*math.pi*band*t_right)
+    y_left= flip(y_right,0)
+
+    ones = torch.ones(1)
+    if cuda:
+        ones = ones.to('cuda')
+    y=torch.cat([y_left, ones, y_right])
+
+    return y
+    
+    
+# Modified from https://github.com/mravanelli/SincNet
+class SincConv(nn.Module):
+
+    def __init__(self, N_filt, Filt_dim, fs,
+                 padding='VALID'):
+        super(SincConv, self).__init__()
+
+        # Mel Initialization of the filterbanks
+        low_freq_mel = 80
+        high_freq_mel = (2595 * np.log10(1 + (fs / 2) \
+                                         / 700))  # Convert Hz to Mel
+        mel_points = np.linspace(low_freq_mel, high_freq_mel, 
+                                 N_filt)  # Equally spaced in Mel scale
+        f_cos = (700 * (10 ** (mel_points / 2595) - 1)) # Convert Mel to Hz
+        b1 = np.roll(f_cos, 1)
+        b2 = np.roll(f_cos, -1)
+        b1[0] = 30
+        b2[-1] = (fs / 2) - 100
+                
+        self.freq_scale=fs * 1.0
+        self.filt_b1 = nn.Parameter(torch.from_numpy(b1/self.freq_scale))
+        self.filt_band = nn.Parameter(torch.from_numpy((b2-b1)/self.freq_scale))
+
+        self.N_filt = N_filt
+        self.Filt_dim = Filt_dim
+        self.fs = fs
+        self.padding = padding
+        
+    def forward(self, x):
+        cuda = x.is_cuda
+        filters=torch.zeros((self.N_filt, self.Filt_dim))
+        N=self.Filt_dim
+        t_right=torch.linspace(1, (N - 1) / 2, 
+                               steps=int((N - 1) / 2)) / self.fs
+        if cuda:
+            filters = filters.to('cuda')
+            t_right = t_right.to('cuda')
+        
+        min_freq=50.0;
+        min_band=50.0;
+        filt_beg_freq = torch.abs(self.filt_b1) + min_freq / self.freq_scale
+        filt_end_freq = filt_beg_freq + (torch.abs(self.filt_band) + \
+                                         min_band / self.freq_scale)
+        n = torch.linspace(0, N, steps = N)
+        # Filter window (hamming)
+        window=(0.54 - 0.46 * torch.cos(2 * math.pi * n / N)).float()
+        if cuda:
+            window = window.to('cuda')
+        for i in range(self.N_filt):
+            low_pass1 = 2 * filt_beg_freq[i].float()* \
+                    sinc(filt_beg_freq[i].float() * self.freq_scale, 
+                         t_right, cuda)
+            low_pass2 = 2 * filt_end_freq[i].float()* \
+                    sinc(filt_end_freq[i].float() * self.freq_scale, 
+                         t_right, cuda)
+            band_pass=(low_pass2 - low_pass1)
+            band_pass=band_pass/torch.max(band_pass)
+            if cuda:
+                band_pass = band_pass.to('cuda')
+
+            filters[i,:]=band_pass * window
+        if self.padding == 'SAME':
+            x_p = F.pad(x, (self.Filt_dim // 2,
+                            self.Filt_dim // 2), mode='reflect')
+        else:
+            x_p = x
+        out = F.conv1d(x_p, filters.view(self.N_filt, 1, self.Filt_dim))
+        return out
+
+
 
 if __name__ == '__main__':
+    """
     import matplotlib
     import matplotlib.pyplot as plt
     import numpy as np
@@ -236,6 +331,11 @@ if __name__ == '__main__':
     z = conv(y)
     print('y: {} -> z: {} conv'.format(y.size(),
                                        z.size()))
+    """
+    x = torch.randn(1, 1, 16384)
+    sincnet = SincConv(1024, 251, 16000, padding='SAME')
+    y = sincnet(x)
+    print('y size: ', y.size())
 
 
 
