44f21a5cd07976388e20ba0c0fc30b196926c2dc
spascual <santi.pdp@gmail.com>
2018-11-13 21:47:55 +0100

    feat: run bash scripts for train/clean of segan+

---
+++ run_segan+_clean.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CKPT_PATH="ckpt_segan+"
+
+# please specify the path to your G model checkpoint
+# as in weights_G-EOE_<iter>.ckpt
+G_PRETRAINED_CKPT=""
+
+# please specify the path to your folder containing
+# noisy test files, each wav in there will be processed
+TEST_FILES_PATH=""
+
+# please specify the output folder where cleaned files
+# will be saved
+SAVE_PATH=""
+
+python -u clean.py --g_pretrained_ckpt $CKPT_PATH/$G_PRETRAINED_CKPT \
+	--test_files $TEST_FILES_PATH --cfg_file $CKPT_PATH/train.opts \
+	--synthesis_path $SAVE_PATH --soundfile
