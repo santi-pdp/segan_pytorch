fb3ec8a32db7950435a261f23991d08b62f5c196
spascual <santi.pdp@gmail.com>
2018-11-14 01:15:10 +0100

    feat: runnable whisper segan train bash script

---
+++ run_wsegan_train.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+python -u train.py --save_path ckpt_wsegan \
+	--clean_trainset data_veu4/silent/clean_trainset_M4 \
+	--noisy_trainset data_veu4/silent/whisper_trainset_M4 \
+	--cache_dir data_silent_tmp --no_train_gen --batch_size 150  \
+	--wsegan --gnorm_type snorm --dnorm_type snorm --opt adam \
+	--data_stride 0.05
